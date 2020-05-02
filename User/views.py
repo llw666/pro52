4b93266090eb7077bab244393c7becd553829371
yun <llw_2019@163.com>
2020-05-02 14:15:09 +0800

    添加视图

--- User/views.py
+++ User/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    name = request.POST.get('name')
+    age = request.POST.get('age')
+
+    user = Users()
+    user.name = name
+    user.age = age
+    user.save()
+    data = {msg:"ok",status:200}
+    return JsonResponse(data=data)
